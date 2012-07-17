c2a4cc1d09e9c023d1e69154133d67a07013719f
Gustavo Medina <gus.a.medina@gmail.com>
2012-07-17 02:28:59 -0400

    add sassy buttons to accomodate for design buttons

--- config.rb
+++ config.rb
@@ -1,10 +1,11 @@
 # Require any additional compass plugins here.
+require 'sassy-buttons'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/css/"
 sass_dir = "public/stylesheets/sass/"
-images_dir = "images"
+images_dir = "public/images"
 javascripts_dir = "public/javascripts/"
 
 # You can select your preferred output style here (can be overridden via the command line):
